8acb0073835179a86a264bd86a26a6061116a6f4
HimaBijulal <g00065495@aus.edu>
2020-04-10 17:58:47 +0400

    removed defective user class

--- app/src/main/java/com/example/homedevices/Login.java
+++ app/src/main/java/com/example/homedevices/Login.java
@@ -34,7 +34,7 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
                     startActivity(intent);
                     System.out.println("NoBlahPls");
                     System.out.println("Blah");
-                    System.out.println("Hello")
+                    System.out.println("Hello");
                     break;
                // }
             }
