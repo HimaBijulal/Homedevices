3c16a105f1570ff2ba8d095fe908d16999c1e231
Hima Bijulal <Hima@Himas-MacBook-Pro.local>
2020-04-10 17:02:04 +0400

    Initial commit

---
+++ app/src/main/java/com/example/homedevices/Login.java
@@ -0,0 +1,42 @@
+package com.example.homedevices;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.EntityIterator;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Login extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText Username, Password;
+    private Button Login;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Username = (EditText) findViewById(R.id.etUsername);
+        Password = (EditText) findViewById(R.id.etPassword);
+        Login = findViewById(R.id.etLoginButton);
+        Login.setOnClickListener(this);
+
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.etLoginButton: {
+                if (Username.getText().toString() == "admin" && Password.getText().toString() == "admin") {
+                    Intent intent = new Intent(this, Success.class);
+                    startActivity(intent);
+                    break;
+                }
+            }
+        }
+    }
+
+
+}
