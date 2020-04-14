d1ce356a4844aab4dbe81f414e1c21273968a0e5
aak231 <Gabriel12341234>
2020-04-14 16:53:17 +0400

    fileio stuff

--- app/src/main/java/com/example/homedevices/Resident.java
+++ app/src/main/java/com/example/homedevices/Resident.java
@@ -3,7 +3,7 @@ package com.example.homedevices;
 
 import java.util.ArrayList;
 
-public class Resident {
+public class Resident  {
 
 	public House House;
 	public ArrayList<Appliance> ApplianceList;
