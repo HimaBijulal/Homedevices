387d062d22b8f014ffbf14144de21370626d5feb
HimaBijulal <g00065495@aus.edu>
2020-04-14 18:30:22 +0400

    addbuilder cancel button

--- app/src/main/java/com/example/homedevices/addbuilder.java
+++ app/src/main/java/com/example/homedevices/addbuilder.java
@@ -2,13 +2,33 @@ package com.example.homedevices;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class addbuilder extends AppCompatActivity {
+public class addbuilder extends AppCompatActivity implements View.OnClickListener{
+
+    private Button cancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addbuilder);
+        cancel = findViewById(R.id.cancelbutton);
+        cancel.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.etLoginButton: {
+                // if (Username.getText().toString() == "admin" && Password.getText().toString() == "admin") {
+                Intent intent = new Intent(this, Dashboard.class);
+                startActivity(intent);
+                break;
+                // }
+            }
+        }
     }
 }
