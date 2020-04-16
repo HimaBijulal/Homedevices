6d4ab285dcddb3e8c1eed5abca04ff60ffadaa72
aak231 <Gabriel12341234>
2020-04-16 19:35:05 +0400

    stuff

--- app/src/main/java/com/example/homedevices/addbuilder.java
+++ app/src/main/java/com/example/homedevices/addbuilder.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.SpannableStringBuilder;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,12 +19,11 @@ public class addbuilder extends AppCompatActivity implements View.OnClickListene
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        FileIO builderfile=new FileIO()
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addbuilder);
         Name = (EditText) findViewById(R.id.edittext5);
         EmiratesID = (EditText) findViewById(R.id.editText);
-        PhoneNo = (EditText) findViewById(R.id.editText2);
+        PhoneNo = Integer.parseInt((EditText) findViewById(R.id.editText2);
         Username = (EditText) findViewById(R.id.editText3);
         Password = (EditText) findViewById(R.id.editText4);
         Add = findViewById(R.id.button2);
@@ -43,8 +43,9 @@ public class addbuilder extends AppCompatActivity implements View.OnClickListene
 
         switch (v.getId()) {
             case R.id.button2: {
+
                 // if (Username.getText().toString() == "admin" && Password.getText().toString() == "admin") {
-                Builder b=new Builder(Name.getText().toString(), EmiratesID.getText()., PhoneNo.getText().toString(), Username.getText().toString(), Password.getText().toString());
+                Builder b=new Builder(Name.getText().toString(), EmiratesID, Integer.parseInt(PhoneNo.getText().toString()), Username.getText().toString(), Password.getText().toString());
                 //Builder c= (Builder) getIntent().getSerializableExtra("User");
                 //System.out.println(c.getEmail()+"");
                 c.AddBuilder(b);
